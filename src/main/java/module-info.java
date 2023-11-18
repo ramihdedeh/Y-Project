da3c26dfe74e997d8dbb8d37f73faecb67aeb32f
ramihdedeh <rami.hdedeh00@gmail.com>
2023-11-18 22:26:52 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.yplatform {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    requires com.almasb.fxgl.all;
+
+    opens Client to javafx.fxml;
+    exports Client;
+}

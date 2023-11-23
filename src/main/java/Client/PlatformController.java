e50978194c4cbce3d96e6ffd55bb0e0f03e91557
ramihdedeh <rami.hdedeh00@gmail.com>
2023-11-23 21:03:36 +0200

    controller of the platform.fxml

---
+++ src/main/java/Client/PlatformController.java
@@ -0,0 +1,35 @@
+package Client;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class PlatformController {
+    @FXML
+    private TextArea User_post;
+
+    @FXML
+    private TextArea New_post;
+
+    @FXML
+    private Button Post_button;
+
+    @FXML
+    private TextArea Messageofinterest;
+
+    @FXML
+    private TextField Followers;
+
+    @FXML
+    private Button Search_users;
+
+    @FXML
+    private Button Reload_messageofinterest;
+
+
+
+
+}

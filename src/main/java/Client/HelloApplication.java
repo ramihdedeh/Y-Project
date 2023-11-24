16c87378ad61d2c467708a23965c28ec9c0b6131
ramihdedeh <115735377+ramihdedeh@users.noreply.github.com>
2023-11-24 13:44:12 +0200

    Update HelloApplication.java

--- src/main/java/Client/HelloApplication.java
+++ src/main/java/Client/HelloApplication.java
@@ -1,5 +1,7 @@
 package Client;
 
+package Client;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,8 +12,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/Client/Login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
@@ -20,4 +22,5 @@ public class HelloApplication extends Application {
     public static void main(String[] args) {
         launch();
     }
+
 }

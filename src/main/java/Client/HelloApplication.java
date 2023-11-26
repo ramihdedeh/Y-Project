a62fcf3f7ecca0569d14179adf5f05d26699953a
ramihdedeh <115735377+ramihdedeh@users.noreply.github.com>
2023-11-26 15:21:44 +0200

    Update HelloApplication.java

--- src/main/java/Client/HelloApplication.java
+++ src/main/java/Client/HelloApplication.java
@@ -7,9 +7,11 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
+
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    private Client Client;
     @Override
     public void start(Stage primaryStage) throws IOException {
         FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource("/Client/Login.fxml"));
@@ -18,7 +20,7 @@ public class HelloApplication extends Application {
 
         LoginController loginController = loader.getController();
 
-        Client Client = new Client();
+        Client = new Client();
         loginController.setClient(Client);
 
         Scene scene = new Scene(root);
@@ -26,6 +28,26 @@ public class HelloApplication extends Application {
         primaryStage.setTitle("Login Page");
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    public void loadPlatformPage(int userId) {
+        try {
+            FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource("/Client/Platform.fxml"));
+            Parent root = loader.load();
+
+            PlatformController platformController = loader.getController();
+            platformController.setUserId(userId);
+
+            Scene scene = new Scene(root);
+
+            Stage stage = (Stage) root.getScene().getWindow(); // Assuming you're using the same stage for now
+
+            stage.setScene(scene);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle exception (e.g., log, show an error message)
+        }
     }
 
     public static void main(String[] args) {

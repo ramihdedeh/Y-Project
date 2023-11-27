4923f58ff90acef0dc06f44197f367c06e63ec1a
ramihdedeh <115735377+ramihdedeh@users.noreply.github.com>
2023-11-27 19:17:06 +0200

    Update PlatformController.java

--- src/main/java/Client/PlatformController.java
+++ src/main/java/Client/PlatformController.java
@@ -6,6 +6,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PlatformController {
@@ -27,10 +30,22 @@ public class PlatformController {
     private TextArea Messageofinterest;
     @FXML
     private FlowPane UserPane;
-    private Client Client;
+    private Client client;
+    private int userId;
+
+    // Getter method for retrieving the user ID
+    public int getUserId() {
+        return userId;
+    }
+
+    // Setter method for setting the user ID
+    public void setUserId(int userId) {
+        this.userId = userId;
+        // Optionally, you can load data based on the user ID here
+    }
 
     public void setClient(Client Client) {
-        this.Client = Client;
+        this.client = client;
     }
 
     private void initialize() {
@@ -39,9 +54,18 @@ public class PlatformController {
         int userId = getUserId(); // Replace with the actual userId
 
         // Fetch recent posts and display them in the User_post TextArea
-        List<String> recentPosts = Client.getRecentPosts(userId);
+        List<String> recentPosts = getRecentPosts(userId);
         for (String post : recentPosts) {
             User_post.appendText(post + "\n");
+        }
+    }
+    public PlatformController() {
+        try {
+            // Assuming your server is running on localhost and port 8000
+            client = new Client("localhost", 8000);
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle the exception (e.g., show an error message)
         }
     }
     @FXML
@@ -55,7 +79,7 @@ public class PlatformController {
         // Check if the message is not empty before sending it
         if (!message.isEmpty()) {
             // Call the sendPostRequest method in the Client class
-            boolean postSuccessful = Client.sendPostRequest(userId, message);
+            boolean postSuccessful = sendPostRequest(userId, message);
 
             // If the post was successful, update the User_post TextArea
             if (postSuccessful) {
@@ -72,73 +96,220 @@ public class PlatformController {
         }
     }
 
-    // Replace this with your actual method for getting the user ID
+    // Method to send post request
+    private boolean sendPostRequest(int userId, String message) {
+        try {
+            // Construct the post request
+            String postRequest = "POSTMESSAGE " + userId + " " + message + "\n";
 
+            // Send the request to the server
+            client.send(postRequest);
 
+            // Receive the response from the server
+            String response = client.receive();
+
+            // Check if the post was successful based on the response
+            return "Message posted successfully!".equals(response);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle exception (e.g., log, show an error message)
+            return false;
+        }
+    }
+
+    private List<String> getRecentPosts(int userId) {
+        try {
+            // Construct the request to get recent posts
+            String getRequest = "MYPOSTS" + userId + "\n";
+
+            // Send the request to the server
+            client.send(getRequest);
+
+            // Receive the response from the server
+            String response = client.receive();
+
+            // Parse the response and return the list of recent posts
+            // You need to implement the logic to parse the response based on your server's protocol
+            // The returned list can be a list of strings, where each string is a post
+            // Example: return List.of("Post 1", "Post 2", "Post 3");
+            // Replace the example with the actual parsing logic.
+
+            // For now, returning an empty list
+            return List.of();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle exception (e.g., log, show an error message)
+            return List.of();
+        }
+    }
     /* this code now is for the followers */
 
+
+
+
+
     @FXML
-    private void handleSearchUsersButton(ActionEvent event) {
-        // Get the search query from the Followers TextField
-        String searchQuery = Followers.getText();
-        int userId = getUserId();
-        // Call the method in Client to search for users
-        List<String> searchResults = Client.searchUsers(searchQuery);
+    private void handleSearchUsersButton() {
+        try {
+            // Get the search query from the Followers TextField
+            String searchQuery = Followers.getText();
 
-        // Display the search results in the Followers TextField
-        Followers.setText(String.join(", ", searchResults));
+            // Construct the search users request
+            String searchRequest = "SEARCH_USERS " + searchQuery + "\n";
 
-        // Check if the user is already a follower and show/hide buttons accordingly
-        boolean isFollower = Client.isFollower(searchQuery);
-        AddFollowerButton.setVisible(!isFollower);
-        RemoveFollowerButton.setVisible(isFollower);
+            // Send the request to the server
+            client.send(searchRequest);
+
+            // Receive the response from the server
+            String response = client.receive();
+
+            // Parse the response and return the list of matching users
+            // Assuming the response is a comma-separated list of usernames
+            String[] usernames = response.split(",");
+            // Display the search results in the Followers TextField
+            Followers.setText(String.join(", ", usernames));
+
+            // Check if the user is already a follower and show/hide buttons accordingly
+            boolean isFollower = isFollower(searchQuery);
+            AddFollowerButton.setVisible(!isFollower);
+            RemoveFollowerButton.setVisible(isFollower);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle exception (e.g., log, show an error message)
+        }
+    }
+
+    private boolean isFollower(String username) {
+        try {
+            // Construct the isFollower request
+            String isFollowerRequest = "IS_FOLLOWER " + username + "\n";
+
+            // Send the request to the server
+            client.send(isFollowerRequest);
+
+            // Receive the response from the server
+            String response = client.receive();
+
+            // Parse the response and return true or false
+            return "true".equals(response);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle exception (e.g., log, show an error message)
+            return false;
+        }
     }
 
     // Method to handle the Add Follower button action
     @FXML
     private void handleAddFollowerButton(ActionEvent event) {
-        // Get the username from the Followers TextField
-        String username = Followers.getText();
+        try {
+            // Get the username from the Followers TextField
+            String username = Followers.getText();
 
-        // Call the method in Client to add the user as a follower
-        boolean addSuccessful = Client.addFollower(username);
+            // Construct the add follower request
+            String addFollowerRequest = "ADDFOLLOWER" + username + "\n";
 
-        // If the user is successfully added, update button visibility
-        if (addSuccessful) {
-            AddFollowerButton.setVisible(false);
-            RemoveFollowerButton.setVisible(true);
+            // Send the request to the server
+            client.send(addFollowerRequest);
+
+            // Receive the response from the server
+            String response = client.receive();
+
+            // Check if the operation was successful based on the response
+            if ("Follower added successfully!".equals(response)) {
+                // If the user is successfully added, update button visibility
+                AddFollowerButton.setVisible(false);
+                RemoveFollowerButton.setVisible(true);
+            } else {
+                // Handle the case where the follower addition failed
+                // You might want to show an alert or provide feedback to the user
+                showAlert("Error", "Failed to add follower. Please try again.");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle exception (e.g., log, show an error message)
         }
     }
 
     // Method to handle the Remove Follower button action
     @FXML
     private void handleRemoveFollowerButton(ActionEvent event) {
-        // Get the username from the Followers TextField
-        String username = Followers.getText();
+        try {
+            // Get the username from the Followers TextField
+            String username = Followers.getText();
 
-        // Call the method in Client to remove the user as a follower
-        boolean removeSuccessful = Client.removeFollower(username);
+            // Construct the remove follower request
+            String removeFollowerRequest = "REMOVEFOLLOWER" + username + "\n";
 
-        // If the user is successfully removed, update button visibility
-        if (removeSuccessful) {
-            RemoveFollowerButton.setVisible(false);
-            AddFollowerButton.setVisible(true);
+            // Send the request to the server
+            client.send(removeFollowerRequest);
+
+            // Receive the response from the server
+            String response = client.receive();
+
+            // Check if the operation was successful based on the response
+            if ("Follower removed successfully!".equals(response)) {
+                // If the user is successfully removed, update button visibility
+                RemoveFollowerButton.setVisible(false);
+                AddFollowerButton.setVisible(true);
+            } else {
+                // Handle the case where the follower removal failed
+                // You might want to show an alert or provide feedback to the user
+                showAlert("Error", "Failed to remove follower. Please try again.");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle exception (e.g., log, show an error message)
         }
     }
 
     @FXML
     private void handleReloadMessageOfInterest(ActionEvent event) {
-        // Get the user ID from wherever you store it (assuming it's available in the PlatformController)
-        int userId = getUserId();
+        try {
+            // Get the user ID from wherever you store it (assuming it's available in the PlatformController)
+            int userId = getUserId();
 
-        // Call the method to get posts from followers from the server
-        List<String> posts = Client.getPostsFromFollowers(userId);
+            // Construct the request to get posts from followers
+            String request = "INTERESTPOSTS" + userId + "\n";
 
-        // Update the Messageofinterest TextArea with the retrieved posts
-        Messageofinterest.clear(); // Clear existing content
-        for (String post : posts) {
-            Messageofinterest.appendText(post + "\n");
+            // Send the request to the server
+            client.send(request);
+
+            // Receive the response from the server
+            String response = client.receive();
+
+            // Process the response and extract posts
+            List<String> posts = parsePostsResponse(response);
+
+            // Update the Messageofinterest TextArea with the retrieved posts
+            Messageofinterest.clear(); // Clear existing content
+            for (String post : posts) {
+                Messageofinterest.appendText(post + "\n");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle exception (e.g., log, show an error message)
         }
+    }
+
+    private List<String> parsePostsResponse(String response) {
+        List<String> posts = new ArrayList<>();
+
+        // Assuming the response contains posts separated by a specific delimiter (adjust as needed)
+        String[] postArray = response.split("\\|"); // Change the delimiter as needed
+
+        for (String post : postArray) {
+            posts.add(post);
+        }
+
+        return posts;
     }
 
     private void showAlert(String title, String content) {
@@ -147,18 +318,6 @@ public class PlatformController {
         alert.setTitle(title);
         alert.setContentText(content);
         alert.showAndWait();
-    }
-    private int userId;
-
-    // Getter method for retrieving the user ID
-    public int getUserId() {
-        return userId;
-    }
-
-    // Setter method for setting the user ID
-    public void setUserId(int userId) {
-        this.userId = userId;
-        // Optionally, you can load data based on the user ID here
     }
 
 
